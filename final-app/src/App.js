f6067ceef5ccb6935c1251fa33db4801039c100c
KharNev <s22210658@student.unklab.ac.id>
2024-11-14 22:50:57 +0800

    update repo

--- final-app/src/App.js
+++ final-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Kelompok Cemara
+          Learn React
         </a>
       </header>
     </div>
